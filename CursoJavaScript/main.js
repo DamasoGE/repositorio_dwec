0434730a0f880202ec8470d9c8d20deb6783f0fb
DamasoGE <franciscodamasogimenez@gmail.com>
2024-10-13 12:35:56 +0200

    13-10-v1

--- CursoJavaScript/main.js
+++ CursoJavaScript/main.js
@@ -4,12 +4,21 @@ import{} from './helpers/ejercicios1.js'
 import{ dec1, buscarPorCiudad } from './helpers/deconstruccion.js'
 import{ users } from './data/usuarios.js'
 import { almacenarlocalstorage } from './helpers/localstore.js'
+import { ejer1, ejer2, ejer3, ejer4, ejer5, ejer6, ejer7} from './helpers/ejercicioslocalstorage.js'
 
 // ---------------Inicio de la Aplicación---------------//
 
 const usuarios = [
-    {nombre:"Damaso", id:33},
-    {nombre:"Pedro", id:66}
+    {nombre:"Damaso", edad:34},
+    {nombre:"Pedro", edad:66},
+    {nombre:"Kevin", edad: 22}
 ]
 
-console.log(almacenarlocalstorage(usuarios,"usuarios"));
+const usuariosmap = new Map(usuarios.map(usuario => [usuario.nombre,usuario.edad]));
+
+console.log(ejer2(usuarios));
+console.log(ejer3("usuarios","Damaso",10))
+console.log(ejer4("usuarios", "Pedro"))
+console.log(ejer5("usuarios"))
+console.log(ejer6(new Set([1,2,4,4,9])))
+console.log(ejer7(usuariosmap))
