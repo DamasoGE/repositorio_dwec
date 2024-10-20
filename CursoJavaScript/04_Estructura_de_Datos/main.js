83194a78f2a4d746f635412e6e4b8e8c8b4639d7
DamasoGE <franciscodamasogimenez@gmail.com>
2024-10-20 23:31:30 +0200

    20-10-v3

--- CursoJavaScript/04_Estructura_de_Datos/main.js
+++ CursoJavaScript/04_Estructura_de_Datos/main.js
@@ -4,7 +4,7 @@ import{} from './helpers/relacion_arrays_1_2.js'
 import{} from './helpers/relacion_basicos.js'
 import{ dec1, buscarPorCiudad } from './helpers/deconstruccion.js'
 import{ users } from './data/usuarios.js'
-import { almacenarlocalstorage } from './helpers/localstore.js'
+import { almacenarlocalstorage, findLS, keyCheck, modifyPrimeroLS, modifyTodosLS, modifyUltimoLS } from './helpers/localstore.js'
 import { ejer1, ejer2, ejer3, ejer4, ejer5, ejer6, ejer7, ejer8, ejer9, ejer10 } from './helpers/relacion_localstorage.js'
 
 // ---------------Inicio de la Aplicación---------------//
@@ -13,13 +13,36 @@ import { ejer1, ejer2, ejer3, ejer4, ejer5, ejer6, ejer7, ejer8, ejer9, ejer10 }
 
 
 // ----------------EJERCICIOS DE LOCALSTORAGE ------------------ //
-// const usuarios = [
-//     {nombre:"Damaso", edad:34},
-//     {nombre:"Pedro", edad:66},
-//     {nombre:"Kevin", edad: 22}
-// ]
+const usuarios = [
+    {nombre:"Damaso", edad:34},
+    {nombre:"Pedro", edad:66},
+    {nombre:"Damaso", edad: 22},
+    {nombre:"Damaso", edad: 22},
+    {nombre:"Damaso", edad: 22}
+]
 
 // const usuariosmap = new Map(usuarios.map(usuario => [usuario.nombre,usuario.edad]));
+
+
+
+almacenarlocalstorage(usuarios,"usuarios");
+
+console.log(keyCheck("usuarios"));
+
+findLS("usuarios","Damaso");
+
+modifyLocal1("usuarios","Damaso","javier")
+
+modifyTodosLS
+
+
+
+
+
+
+
+
+
 
 // console.log(ejer2(usuarios));
 // console.log(ejer3("usuarios","Damaso",10))
