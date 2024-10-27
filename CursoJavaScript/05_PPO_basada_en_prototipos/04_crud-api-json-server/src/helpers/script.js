825e2a9b4c2be6eb83950d73af0ebe89b31c2ea1
DamasoGE <franciscodamasogimenez@gmail.com>
2024-10-27 18:52:51 +0100

    27-10-v3

--- CursoJavaScript/05_PPO_basada_en_prototipos/04_crud-api-json-server/src/helpers/script.js
+++ CursoJavaScript/05_PPO_basada_en_prototipos/04_crud-api-json-server/src/helpers/script.js
@@ -1,10 +1,7 @@
-
-const dataUrl = "http://localhost:4000/historialPrecios"
-
 /**
- * 
+ * @description: CREATE
  */
-export const insertData = async (newData) =>{
+export const createData = async (dataUrl, newData) =>{
     try {
         const response = await fetch(dataUrl, {
             method: 'POST',
@@ -20,6 +17,72 @@ export const insertData = async (newData) =>{
         console.log("Producto creado correctamente ", data);
         return data;
     } catch (error) {
+        console.log("Error al obtener los datos ", error)
+    }
+}
+
+
+/**
+ * 
+ * @description: READ
+ */
+export const readData = async (dataUrl) => {
+    try {
+        const response = await fetch(dataUrl)
+        if(!response.ok){
+            throw new Error("Error al obtener la respuesta")
+        }
+        const data = await response.json();
+        console.log("Leídos los datos:", data);
+        // return data;
+    }catch (error) {
+        console.log("Error al obtener los datos ", error)
+    }
+}
+
+/**
+ * @description: UPDATE
+ */
+export const updateData = async (dataUrl, id, updData) => {
+    try {
+        const response = await fetch(`${dataUrl}/${id}`, {
+            method: 'PUT', //EXISTE EL PATH PARA EDITAR VALORES CONCRETOS
+            headers: {
+                'Content-Type' : 'aplication/JSON'
+            },
+            body: JSON.stringify(updData)
+        })
+        if(!response.ok){
+            throw new Error("Error al obtener la respuesta")
+        }
+        const data = await response.json();
+        console.log("Entrada modificada:", data);
+        // return data;
+    }catch (error) {
+        console.log("Error al obtener los datos ", error)
+    }
+}
+
+/**
+ * @description: DELETE
+ */
+export const deleteData = async (dataUrl, id) => {
+    try {
+        const response = await fetch(`${dataUrl}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type' : 'aplication/JSON'
+            },
+        })
+        if(!response.ok){
+            throw new Error("Error al obtener la respuesta")
+        }
+        const data = await response.json();
+
+
+        console.log("Producto borrado correctamente ", data);
+        // return data;
+    }catch (error) {
         console.log("Error al obtener los datos ", error)
     }
 }
--- CursoJavaScript/05_PPO_basada_en_prototipos/04_crud-api-json-server/src/main.js
+++ CursoJavaScript/05_PPO_basada_en_prototipos/04_crud-api-json-server/src/main.js
@@ -1,8 +1,28 @@
-import { insertData } from "./helpers/script";
+import { createData, readData, updateData, deleteData } from "./helpers/script";
 
-const dataParaInsertar = {
-    productId: 1,
+//--------------------CREATE-----------------//
+// const dataUrl = "http://localhost:4000/historialPrecios"
+// const data = {
+//     productoId: 2,
+//     precios: [23,26,21.75,19.99]
+// }
+// createData(dataUrl, data)
+
+//------------------READ---------------------//
+// const dataUrl = "http://localhost:4000/historialPrecios"
+// readData(dataUrl);
+
+//------------------UPDATE------------------//
+const dataUrl = "http://localhost:4000/historialPrecios"
+const idData = "49a6";
+const data = {
+    productoId: 8,
     precios: [23,26,21.75,19.99]
 }
+updateData(dataUrl,"49a6",data)
 
-insertData(dataParaInsertar)
+//--------------------DELETE----------------//
+// const dataUrl = "http://localhost:4000/historialPrecios"
+// const idData = "c3bb";
+// deleteData(dataUrl, idData);
+
